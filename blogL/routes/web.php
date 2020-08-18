7931d24c4f381d0624b8c6514e91e3bef0c3530e
Wai Yan Kyaw <scm.waiyankyaw@gmail.com>
2020-08-18 12:38:26 +0630

    voyager user updated

--- blogL/routes/web.php
+++ blogL/routes/web.php
@@ -28,6 +28,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::group(['prefix' => 'voyager'], function () {
+Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
